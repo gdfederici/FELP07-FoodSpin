54de7ce219a0394a11004b4e7706b5917b1ced52
gdfederici <gdfederici@gmail.com>
2021-05-24 17:32:11 +0200

    Smaller function for index.js.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,25 +10,32 @@ import imgFood6 from '../img/food-big_6.png';
 import imgFood7 from '../img/food-big_7.png';
 import imgFood8 from '../img/food-big_8.png';
 import imgFood9 from '../img/food-big_9.png';
+//import thumbFood0 from '../img/food-small_0.png';
+//import thumbFood1 from '../img/food-small_1.png';
+//import thumbFood2 from '../img/food-small_2.png';
+//import thumbFood3 from '../img/food-small_3.png';
+//import thumbFood4 from '../img/food-small_4.png';
+//import thumbFood5 from '../img/food-small_5.png';
+//import thumbFood6 from '../img/food-small_6.png';
+//import thumbFood7 from '../img/food-small_7.png';
+//import thumbFood8 from '../img/food-small_8.png';
+//import thumbFood9 from '../img/food-small_9.png';
 import imgArrow from '../img/arrow-down.png';
 import dishesJson from './dishes.json';
 
 /*** ------------------------- *** ------------------------- *** ------------------------- *** ------------------------- ***/
 
 /*** DISHES ***/
-window.isDishes = function() {
+window.isFood = function() {
 	// IT_ Creo i piatti prendendole dal file json esterno. | EN_ Create dishes taking them from the external json file.
 	let listDishes = dishesJson.dishesList;
-	console.log("listDishes", listDishes);
-    let ringaRinga = document.createElement("ul");
-    ringaRinga.className = "menu";
+    let foodList = document.createElement("ul");
+    foodList.className = "menu";
     let allDishes = listDishes.length;
-    console.log("allDishes", allDishes);
     for (let i=0; i<allDishes; i++) {
-        ringaRinga.appendChild(createDish(listDishes[i].price, listDishes[i].title, listDishes[i].description, listDishes[i].image, i));
+        foodList.appendChild(createDish(listDishes[i].price, listDishes[i].title, listDishes[i].description, listDishes[i].image, i));
     }
-    document.getElementById("main").appendChild(ringaRinga);
-    return;
+    document.getElementById("main").appendChild(foodList);
 }
 
 // IT_ Visualizza il singolo piatto della lista. | EN_ View the single dish on the list.
@@ -38,7 +45,7 @@ function createDish(itemPrice, itemTitle, itemDescription, itemImage, position) 
     (position %2) === 0 ?  (color = "a") : (color = "b");
     listItem.className = "spin spin__color-"+ color;
     listItem.appendChild(createSX(itemPrice, itemTitle, itemDescription));
-    listItem.appendChild(createDX(itemImage));
+    listItem.appendChild(createDX(itemImage, position));
     return listItem;
 }
 
@@ -46,59 +53,92 @@ function createDish(itemPrice, itemTitle, itemDescription, itemImage, position) 
 function createSX(itemPrice, itemTitle, itemDescription) {
     let listItemSX = document.createElement("div");
     listItemSX.className = "spin-sx";
-    let listItemPrice = document.createElement("p");
-    listItemPrice.className = "spin-sx-price price";
-    listItemPrice.appendChild(document.createTextNode(itemPrice));
-    listItemSX.appendChild(listItemPrice);
-    let listItemTitle = document.createElement("h1");
-    listItemTitle.className = "spin-sx-title";
-    listItemTitle.appendChild(document.createTextNode(itemTitle));
-    listItemSX.appendChild(listItemTitle);
-    let listItemDescription = document.createElement("p");
-    listItemDescription.className = "spin-sx-text";
-    listItemDescription.appendChild(document.createTextNode(itemDescription))
-    listItemSX.appendChild(listItemDescription);
-    let listItemOrder = document.createElement("div");
-    listItemOrder.className = "spin-sx-button";
-    let listItemButton = document.createElement("button");
-    listItemButton.className = "button";
-    listItemButton.appendChild(document.createTextNode("Order Now"));
-    listItemOrder.appendChild(listItemButton);
-    listItemSX.appendChild(listItemOrder);
+    // IT_ Prezzo. | EN_ Price.
+    listItemSX.appendChild(createPrice(itemPrice));
+    // IT_ Titolo. | EZ_ Title.
+    listItemSX.appendChild(createTitle(itemTitle));
+    // IT_ Descrizione. | EN_ Description.
+    listItemSX.appendChild(createDescription(itemDescription));
+    // IT_ Bottone. | EN_ Button.
+    listItemSX.appendChild(createButton());
+    //
     return listItemSX;
+}
+// IT_ Prezzo. | EN_ Price.
+function createPrice(price) {
+    let itemPrice = document.createElement("p");
+    itemPrice.className = "spin-sx-price price";
+    itemPrice.appendChild(document.createTextNode(price));
+    return itemPrice;
+}
+// IT_ Titolo. | EZ_ Title.
+function createTitle(title) {
+    let itemTitle = document.createElement("h1");
+    itemTitle.className = "spin-sx-title";
+    itemTitle.appendChild(document.createTextNode(title));
+    return itemTitle;
+}
+// IT_ Descrizione. | EN_ Description.
+function createDescription(description) {
+    let itemDescription = document.createElement("p");
+    itemDescription.className = "spin-sx-text";
+    itemDescription.appendChild(document.createTextNode(description));
+    return itemDescription;
+}
+// IT_ Bottone. | EN_ Button.
+function createButton() {
+    let itemOrder = document.createElement("div");
+    itemOrder.className = "spin-sx-button";
+    let itemButton = document.createElement("button");
+    itemButton.className = "button";
+    itemButton.appendChild(document.createTextNode("Order Now"));
+    itemOrder.appendChild(itemButton);
+    return itemOrder;
 }
 
 // IT_ Crea la parte DX con ruota dei piatti e immagine piatto principale. | EN_ Create the DX part with the dishes wheel and main food image.
-function createDX(itemImage) {
+function createDX(itemImage, position) {
     let listItemDX = document.createElement("div");
     listItemDX.className = "spin-dx";
-    let listItemCircle = document.createElement("div");
-    listItemCircle.className = "spin-dx-infernalcircle background";
-    let listItemWheel = document.createElement("div");
-    listItemWheel.className = "spin-dx-wheel";
-    listItemWheel.appendChild(createImage("ruotacibo", imgFoodCircle, "657", "657", "Circle food"));
-    listItemCircle.appendChild(listItemWheel);
-    listItemDX.appendChild(listItemCircle);
-    let listItemFood = document.createElement("div");
-    listItemFood.className = "spin-dx-food";
-    listItemFood.appendChild(createImage("", "img/" + itemImage, "289", "295", "Main Food"));
-    listItemDX.appendChild(listItemFood);
-    let listItemControls = document.createElement("div");
-    listItemControls.className = "spin-controls arrows";
-    let listItemPrev = document.createElement("button");
-    listItemPrev.className = "prev";
-    listItemPrev.setAttribute("id", "arrowPrev");
-    listItemPrev.onclick = function() { spinWheel(-1); };
-    listItemPrev.appendChild(createImage("", imgArrow, "14", "14", "Spin arrow"));
-    let listItemNext = document.createElement("button");
-    listItemNext.className = "next";
-    listItemNext.setAttribute("id", "arrowNext");
-    listItemNext.onclick = function() { spinWheel(1); };
-    listItemNext.appendChild(createImage("", imgArrow, "14", "14", "Spin arrow"));
-    listItemControls.appendChild(listItemPrev);
-    listItemControls.appendChild(listItemNext);
-    listItemDX.appendChild(listItemControls);
+    // IT_Sfondo e ruota. | EN_ Background and wheel.
+    listItemDX.appendChild(createWheel(position));
+    // IT_ Piatto principale. | EN_ Single food.
+    listItemDX.appendChild(createDishOne(itemImage));
+    // IT_ Controlli per spin. | EN_ Spin controls.
+    listItemDX.appendChild(createControls(imgArrow));
+    //
     return listItemDX;
+}
+// IT_Sfondo e ruota. | EN_ Background and wheel.
+function createWheel(index) {
+    // IT_ Sfondo che nasconde. | EN_ Background and overflow hidden.
+    let itemCircle = document.createElement("div");
+    itemCircle.className = "spin-dx-infernalcircle background";
+    // IT_ Ruota dei piatti. | EN_ Circle of dishes.
+    itemCircle.appendChild(createCircle(index));
+    return itemCircle;
+}
+// IT_ Crea il cerchio di cibi presente nello sfondo. | EN_ Create dishes'circle for background.
+function createCircle(imgPosition) {
+    let itemWheel = document.createElement("div");
+    itemWheel.className = "spin-dx-wheel";
+    let itemWheel_back = document.createElement("div");
+    itemWheel_back.className = "spin-dx-wheel_backcircle";
+    itemWheel.appendChild(itemWheel_back);
+    for (let j=0; j<10; j++) {
+        let itemWheel_dish = document.createElement("div");
+        itemWheel_dish.className = "spin-dx-wheel_dish spin-dx-wheel_dish-" + j;
+        itemWheel_dish.style.transform = "rotate(" + ((imgPosition-j) * 36) +"deg)";
+        itemWheel.appendChild(itemWheel_dish);
+    }
+    return itemWheel;
+}
+// IT_ Piatto principale. | EN_ Single food.
+function createDishOne(itemImage) {
+    let itemFood = document.createElement("div");
+    itemFood.className = "spin-dx-food";
+    itemFood.appendChild(createImage("", "img/" + itemImage, "289", "295", "Main Food"));
+    return itemFood;
 }
 // IT_ Crea l'elemento immagine da inserire. | EN_ Create the image element to insert.
 function createImage(imgClass, imgSrc, imgWidth, imgHeight, imgAlt) {
@@ -110,6 +150,23 @@ function createImage(imgClass, imgSrc, imgWidth, imgHeight, imgAlt) {
     newImage.loading = "lazy";
     newImage.alt = imgAlt;
     return newImage;
+}
+// IT_ Controlli per spin. | EN_ Spin controls.
+function createControls(imgArrow) {
+    let itemControls = document.createElement("div");
+    itemControls.className = "spin-controls arrows";
+    itemControls.appendChild(createArrowControl(imgArrow, "prev", "arrowPrev", -1));
+    itemControls.appendChild(createArrowControl(imgArrow, "next", "arrowNext", 1));
+    return itemControls;
+}
+// IT_ Bottone freccia. | EN_ Arrow button.
+function createArrowControl(imgArrow, classArrow, idArrow, x) {
+    let itemArrow = document.createElement("button");
+    itemArrow.className = classArrow;
+    itemArrow.setAttribute("id", idArrow);
+    itemArrow.onclick = function() { spinWheel(x); };
+    itemArrow.appendChild(createImage("", imgArrow, "14", "14", "Spin arrow"));
+    return itemArrow;
 }
 
 
@@ -136,8 +193,7 @@ function magicabula(itemShow) {
 }
 
 
-
 /*** MAIN ***/
-isDishes();
+isFood();
 var foodIndex = 1;
-showFood(foodIndex);
+showFood(foodIndex, 0);
